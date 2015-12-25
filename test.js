03ef53f364b2b184187d10c19c9280f309afd281
Gabriel Montalvo <gabe.mon5@outlook.com>
2015-12-25 01:51:10 -0400

    tab fix
    I was mixing tabs with spaces. Also previous commit title had a typo
    (trivial *).

--- test.js
+++ test.js
@@ -3,9 +3,9 @@ var test = require('ava');
 var kbpk = require('./');
 
 test(function (t) {
-	t.plan(1);
+  t.plan(1);
 
-	kbpk('chris', function (public_key) {
-		t.assert(typeof public_key === 'string');
-	});
+  kbpk('chris', function (public_key) {
+    t.assert(typeof public_key === 'string');
+  });
 });
